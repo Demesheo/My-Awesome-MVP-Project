69636576d1c846e2bdea1636e7d3536220336658
Demesheo Wilson <demesheochevez@icloud.com>
2015-08-04 10:41:51 -0700

    mvp product complete, need to style and add generate pop data

--- app.js
+++ app.js
@@ -1,50 +1,34 @@
 $(document).ready(function(){
- //Creating our new map at Telegraph Academy's coordinates	
+ //Creating our new default map centered at Telegraph Academy's coordinates	
  var map = new GMaps({
       div: '#map',
-      lat: 37.878012, 
-      lng: -122.269621,
-      width: '1000px',
+      lat: 37.877, 
+      lng: -122.27,
+      zoom: 18,
+      width: '960px',
       height: '500px'
   });
 
-
-
- //We want to connect to our mongo database, using express
  var pastPosts;
  var allPosts = [];
 
  var getPosts = function(){
-
- if(Lockr.get('posts')!==undefined){	
+ if(Lockr.get('posts') !== undefined){	
  	pastPosts = Lockr.get('posts');
- 	allPosts.push(pastPosts)
- //We want to perform a GET request to get all of our stored geolocs and messages from the server
- //We then want to addMarkers for each geoloc from the GET response with corresponding messages to populate our map
- if(pastPosts.length){
- 	for(var i = 0; i < pastPosts.length; i++){
- 		map.addMarker(pastPosts[i]);
- 	};
+  console.log("past posts : ", pastPosts);
+
+ for(var i = 0; i < pastPosts.length; i++){
+  map.addMarker(pastPosts[i]);
  };
 };
 };
-
-
- //When we submit our text box, we want to run geolocate and post our location and message to the local storage
- 	//Then we will perform a GET request to repopulate our map with the new post
- //Use Gmaps geolocate function to show our current location
+//When we submit our text box, we want to run geolocate and post our location and message to the local storage
+//Then we will perform a GET request to repopulate our map with the new post
+//Use Gmaps geolocate function to show our current location
  GMaps.geolocate({
     success: function(position){
       map.setCenter(position.coords.latitude, position.coords.longitude);
       getPosts();
-      // map.addMarker({
-      //   lat: position.coords.latitude,
-      //   lng: position.coords.longitude,
-      //   title: 'You are here.'
-      //   // infoWindow: {
-      //   //   content: '<p>You are here!</p>'
-      //   // }
-      // });
     },
     error: function(error){
       alert('Geolocation failed: '+error.message);
@@ -54,13 +38,9 @@ $(document).ready(function(){
     }
   });
 //End of map functions
-//Begin user functions
-
-    
+//Begin user post function
     $("button").click(function(){
-	
 	var message = $('input').val();
-    
     GMaps.geolocate({
      success: function(position){
      	var newPost = {
@@ -71,18 +51,17 @@ $(document).ready(function(){
      			content: '<p>'+message+'</p>'
      		}
      	};
-
-     	allPosts.push(newPost);
-     	Lockr.set('posts', allPosts);
+     	pastPosts.push(newPost);
+     	Lockr.set('posts', pastPosts);
      	getPosts();
-     	// map.addMarker({
-     	// 	lat: position.coords.latitude,
-     	// 	lng: position.coords.longitude,
-     	// 	title: 'One of your posts.',
-     	// 	infoWindow: {
-     	// 		content: '<p>'+message+'</p>'
-     	// 	}
-     	// });
+     	map.addMarker({
+     		lat: position.coords.latitude,
+     		lng: position.coords.longitude,
+     		title: 'One of your posts.',
+     		infoWindow: {
+     			content: '<p>'+message+'</p>'
+     		}
+     	});
      },
      error: function(error){
       alert('Geolocation failed: '+error.message);
@@ -90,11 +69,6 @@ $(document).ready(function(){
     not_supported: function(){
       alert("Your browser does not support geolocation");
     }
-   });	
-
-	
+   });
 	});
-
-
-
 });
